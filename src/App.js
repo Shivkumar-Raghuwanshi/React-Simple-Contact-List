92c335637415f68d3cdcdb0c8c90e6bbf599f569
Shivkumar-Raghuwanshi <shivkumarraghuwanshi56@gmail.com>
2023-10-09 09:29:40 +0530

    update

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ const App = () => {
 
   useEffect(() => {
 
-    fetch("https://randomuser.me/api/?results=10")
+    fetch("https://randomuser.me/api/?results=100")
       .then(response => response.json())
       .then(data => {
         console.log(data);
